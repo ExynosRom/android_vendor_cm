afbe8e6066c3673021b3f4a128d07c39d33ecfed
John Wu <topjohnwu@gmail.com>
2017-06-03 19:20:59 +0200

    vendor: Switch to Magisk for root solution [2/2] This will add the Magisk Manager into the ROM and move the zip to be flashed during ROM installation. Solution is stable and actively supported by its creator. Commit actually written by @nathanchance, with authorship given to topjohnwu since he did everything that is being added.
    Start with Magisk v11.1 & Magisk Manager v4.1
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    
    Conflicts:
    	configs/packages.mk
    
    Change-Id: I861d6cf9b3140a411611f1f212b359b9f78ccbc8

--- config/common.mk
+++ config/common.mk
@@ -198,6 +198,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     rsync
 
+# Magisk Manager
+ PRODUCT_PACKAGES += \
+     MagiskManager
+ 
+ # Copy Magisk zip
+ PRODUCT_COPY_FILES += \
+     vendor/cm/prebuilt/zip/magisk.zip:system/addon.d/magisk.zip
+ 
+ 	
+	
 # Stagefright FFMPEG plugin
 PRODUCT_PACKAGES += \
     libffmpeg_extractor \
